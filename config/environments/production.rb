3160018cf1cdf0104820c0c1432c4cfc35a87d67
fumi <381704fumi@gmail.com>
2020-07-23 09:12:49 +0000

    for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # actioncableの使用による追加
-  # config.action_cable.allowed_request_origins = [ 'http://ones-place.work', /http:\/\/ones-place.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://ones-place.work', /http:\/\/ones-place.*/ ]
+  
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # https化
