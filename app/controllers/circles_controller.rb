bc00c113ba893f45870ce3683b351eda8c8cd28b
fumi <381704fumi@gmail.com>
2020-08-11 00:47:26 +0900

    circle関係のビジュアル変更

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -80,8 +80,9 @@ class CirclesController < ApplicationController
   end
 
   def set_places
-    # @places = PLACES
-    @places = %w[快晴 晴れ 薄曇り 曇り 小雨 雨 豪雨 雷 みぞれ 雪 大雪 あられ ひょう 霧 霧雨 砂あらし]
+    @places = %w[北海道 青森県 岩手県 宮城県 秋田県 山形県 福島県 茨城県 栃木県 群馬県 埼玉県 千葉県 東京都 神奈川県 山梨県 長野県
+      新潟県 富山県 石川県 福井県 岐阜県 静岡県 愛知県 三重県 滋賀県 京都府 大阪府 兵庫県 奈良県 和歌山県 鳥取県 島根県 岡山県 広島県
+      山口県 徳島県 香川県 愛媛県 高知県 福岡県 佐賀県 長崎県 熊本県 大分県 宮崎県 鹿児島県 沖縄県 その他]
   end
 
   def search
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create!(name: 'Luke', movie: movies.first)
-
 Category.create!([
     { kind: '身体を動かす' },
     { kind: '勉強する' },
@@ -16,57 +11,6 @@ Category.create!([
     { kind: 'これが食べたい' },
     { kind: '新しく創る' }
   ])
-
-# Circle.create!([
-#     { place: '北海道' },
-#     { place: '青森県' },
-#     { place: '岩手県' },
-#     { place: '宮城県' },
-#     { place: '秋田県' },
-#     { place: '山形県' },
-#     { place: '福島県' },
-#     { place: '茨城県' },
-#     { place: '栃木県' },
-#     { place: '群馬県' },
-#     { place: '埼玉県' },
-#     { place: '千葉県' },
-#     { place: '東京都' },
-#     { place: '神奈川県' },
-#     { place: '山梨県' },
-#     { place: '長野県' },
-#     { place: '新潟県' },
-#     { place: '富山県' },
-#     { place: '石川県' },
-#     { place: '福井県' },
-#     { place: '岐阜県' },
-#     { place: '静岡県' },
-#     { place: '愛知県' },
-#     { place: '三重県' },
-#     { place: '滋賀県' },
-#     { place: '京都府' },
-#     { place: '大阪府' },
-#     { place: '兵庫県' },
-#     { place: '奈良県' },
-#     { place: '和歌山県' },
-#     { place: '鳥取県' },
-#     { place: '島根県' },
-#     { place: '岡山県' },
-#     { place: '広島県' },
-#     { place: '山口県' },
-#     { place: '徳島県' },
-#     { place: '香川県' },
-#     { place: '愛媛県' },
-#     { place: '高知県' },
-#     { place: '福岡県' },
-#     { place: '佐賀県' },
-#     { place: '長崎県' },
-#     { place: '熊本県' },
-#     { place: '大分県' },
-#     { place: '宮崎県' },
-#     { place: '鹿児島県' },
-#     { place: '沖縄県' },
-#     { place: 'その他'}
-#   ])
 
 if Rails.env == 'development'
   30.times do |i|
@@ -86,16 +30,5 @@ following = users[2..30]
 followers = users[3..20]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
-
-#   30.times do |i|
-#     no = i + 1
-#     circle = Circle.create!(
-#       user_id: no,
-#       name: "user_#{no}",
-#       content: "content_#{no}",
-#       image_id: nill
-#     )
-#     circle.save!
-#   end
 
 end
