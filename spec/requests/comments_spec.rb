27771a044a9aa5f1b9b5b558fe5eef2761aff48b
fumi <381704fumi@gmail.com>
2020-08-29 14:02:15 +0900

    テストの追加と修正

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,4 +1,51 @@
 require 'rails_helper'
 
-# RSpec.describe CommentsController, type: :controller do
-# end
+require 'rails_helper'
+
+RSpec.describe 'Comments', type: :request do
+  let!(:user) { create(:user) }
+  let!(:circle) { create(:circle, user: user) }
+  let!(:blog) { create(:blog, user: user, circle: circle) }
+  let(:comment) { create(:comment, blog: blog) }
+
+  describe '#create' do
+    context 'ログインしている場合' do
+      before { sign_in user }
+
+      it '正常に作成できること' do
+        expect do
+          post comments_path, params: { post: comment }
+        end.to change { blog.comments.count }.by(1)
+      end
+    end
+
+    context 'ログインしていない場合' do
+      before { post comments_path, params: { post: comment } }
+
+      it 'ログインページにリダイレクトされること' do
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe '#destroy' do
+    let!(:comment) { create(:comment, blog: blog) }
+
+    context '本人の場合' do
+      before { sign_in user }
+
+      it '正常に削除できること' do
+        expect do
+          delete comment_path(comment)
+        end.to change { blog.comments.count }.by(-1)
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'ログインページにリダイレクトされること' do
+        delete comment_path(comment)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+end
